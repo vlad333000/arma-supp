3faf0fb41bd9da3f7f721f3cd7f74733f8e3c682
Vladislav Sazonov <vladsazonov@hotmail.com>
2019-09-02 02:57:21 +0300

    Marker brushes Extension

---
+++ GUI/Map/Marker brushes/configs/CfgMarkerBrushes.hpp
@@ -0,0 +1,191 @@
+class CfgMarkerBrushes {
+    class ASUP_Chess_Big {
+        name = $STR_ASUP_CfgMarkerBrushes_Chess_Big;
+        texture = "\ASUP\CfgMarkerBrushes\data\chess_big_ca.paa";
+        scope = 1;
+        drawBorder = 0;
+    };
+    class ASUP_Chess_Medium {
+        name = $STR_ASUP_CfgMarkerBrushes_Chess_Medium;
+        texture = "\ASUP\CfgMarkerBrushes\data\chess_medium_ca.paa";
+        scope = 1;
+        drawBorder = 0;
+    };
+    class ASUP_Chess_Small {
+        name = $STR_ASUP_CfgMarkerBrushes_Chess_Small;
+        texture = "\ASUP\CfgMarkerBrushes\data\chess_small_ca.paa";
+        scope = 1;
+        drawBorder = 0;
+    };
+
+    class ASUP_Circles_Big {
+        name = $STR_ASUP_CfgMarkerBrushes_Circles_Big;
+        texture = "\ASUP\CfgMarkerBrushes\data\circles_big_ca.paa";
+        scope = 1;
+        drawBorder = 0;
+    };
+    class ASUP_Circles_Medium {
+        name = $STR_ASUP_CfgMarkerBrushes_Circles_Medium;
+        texture = "\ASUP\CfgMarkerBrushes\data\circles_medium_ca.paa";
+        scope = 1;
+        drawBorder = 0;
+    };
+    class ASUP_Circles_Small {
+        name = $STR_ASUP_CfgMarkerBrushes_Circles_Small;
+        texture = "\ASUP\CfgMarkerBrushes\data\circles_small_ca.paa";
+        scope = 1;
+        drawBorder = 0;
+    };
+
+    class ASUP_Dots {
+        name = $STR_ASUP_CfgMarkerBrushes_Dots;
+        texture = "\ASUP\CfgMarkerBrushes\data\dots_ca.paa";
+        scope = 1;
+        drawBorder = 0;
+    };
+
+    class ASUP_Line_Diagonal_Backward {
+        name = $STR_ASUP_CfgMarkerBrushes_Line_Diagonal_Backward;
+        texture = "\ASUP\CfgMarkerBrushes\data\line_diagonal_backward_ca.paa";
+        scope = 1;
+        drawBorder = 0;
+    };
+    class ASUP_Line_Diagonal_Forward {
+        name = $STR_ASUP_CfgMarkerBrushes_Line_Diagonal_Forward;
+        texture = "\ASUP\CfgMarkerBrushes\data\line_diagonal_forward_ca.paa";
+        scope = 1;
+        drawBorder = 0;
+    };
+    class ASUP_Line_Horizontal {
+        name = $STR_ASUP_CfgMarkerBrushes_Line_Horizontal;
+        texture = "\ASUP\CfgMarkerBrushes\data\line_horizontal_ca.paa";
+        scope = 1;
+        drawBorder = 0;
+    };
+    class ASUP_Line_Vertical {
+        name = $STR_ASUP_CfgMarkerBrushes_Line_Vertical;
+        texture = "\ASUP\CfgMarkerBrushes\data\line_vertical_ca.paa";
+        scope = 1;
+        drawBorder = 0;
+    };
+
+    class ASUP_Lines_Diagonal_Backward_Big {
+        name = $STR_ASUP_CfgMarkerBrushes_Lines_Diagonal_Backward_Big;
+        texture = "\ASUP\CfgMarkerBrushes\data\lines_diagonal_backward_big_ca.paa";
+        scope = 1;
+        drawBorder = 0;
+    };
+    class ASUP_Lines_Diagonal_Backward_Medium {
+        name = $STR_ASUP_CfgMarkerBrushes_Lines_Diagonal_Backward_Medium;
+        texture = "\ASUP\CfgMarkerBrushes\data\lines_diagonal_backward_medium_ca.paa";
+        scope = 1;
+        drawBorder = 0;
+    };
+    class ASUP_Lines_Diagonal_Backward_Small {
+        name = $STR_ASUP_CfgMarkerBrushes_Lines_Diagonal_Backward_Small;
+        texture = "\ASUP\CfgMarkerBrushes\data\lines_diagonal_backward_small_ca.paa";
+        scope = 1;
+        drawBorder = 0;
+    };
+
+    class ASUP_Lines_Diagonal_Forward_Big {
+        name = $STR_ASUP_CfgMarkerBrushes_Lines_Diagonal_Forward_Big;
+        texture = "\ASUP\CfgMarkerBrushes\data\lines_diagonal_forward_big_ca.paa";
+        scope = 1;
+        drawBorder = 0;
+    };
+    class ASUP_Lines_Diagonal_Forward_Medium {
+        name = $STR_ASUP_CfgMarkerBrushes_Lines_Diagonal_Forward_Medium;
+        texture = "\ASUP\CfgMarkerBrushes\data\lines_diagonal_forward_medium_ca.paa";
+        scope = 1;
+        drawBorder = 0;
+    };
+    class ASUP_Lines_Diagonal_Forward_Small {
+        name = $STR_ASUP_CfgMarkerBrushes_Lines_Diagonal_Forward_Small;
+        texture = "\ASUP\CfgMarkerBrushes\data\lines_diagonal_forward_small_ca.paa";
+        scope = 1;
+        drawBorder = 0;
+    };
+
+    class ASUP_Lines_Horizontal_Large {
+        name = $STR_ASUP_CfgMarkerBrushes_Lines_Horizontal_Large;
+        texture = "\ASUP\CfgMarkerBrushes\data\lines_horizontal_large_ca.paa";
+        scope = 1;
+        drawBorder = 0;
+    };
+    class ASUP_Lines_Horizontal_Big {
+        name = $STR_ASUP_CfgMarkerBrushes_Lines_Horizontal_Big;
+        texture = "\ASUP\CfgMarkerBrushes\data\lines_horizontal_big_ca.paa";
+        scope = 1;
+        drawBorder = 0;
+    };
+    class ASUP_Lines_Horizontal_Medium {
+        name = $STR_ASUP_CfgMarkerBrushes_Lines_Horizontal_Medium;
+        texture = "\ASUP\CfgMarkerBrushes\data\lines_horizontal_medium_ca.paa";
+        scope = 1;
+        drawBorder = 0;
+    };
+    class ASUP_Lines_Horizontal_Small {
+        name = $STR_ASUP_CfgMarkerBrushes_Lines_Horizontal_Small;
+        texture = "\ASUP\CfgMarkerBrushes\data\lines_horizontal_small_ca.paa";
+        scope = 1;
+        drawBorder = 0;
+    };
+
+    class ASUP_Lines_Vertical_Large {
+        name = $STR_ASUP_CfgMarkerBrushes_Lines_Vertical_Large;
+        texture = "\ASUP\CfgMarkerBrushes\data\lines_vertical_large_ca.paa";
+        scope = 1;
+        drawBorder = 0;
+    };
+    class ASUP_Lines_Vertical_Big {
+        name = $STR_ASUP_CfgMarkerBrushes_Lines_Vertical_Big;
+        texture = "\ASUP\CfgMarkerBrushes\data\lines_vertical_big_ca.paa";
+        scope = 1;
+        drawBorder = 0;
+    };
+    class ASUP_Lines_Vertical_Medium {
+        name = $STR_ASUP_CfgMarkerBrushes_Lines_Vertical_Medium;
+        texture = "\ASUP\CfgMarkerBrushes\data\lines_vertical_medium_ca.paa";
+        scope = 1;
+        drawBorder = 0;
+    };
+    class ASUP_Lines_Vertical_Small {
+        name = $STR_ASUP_CfgMarkerBrushes_Lines_Vertical_Small;
+        texture = "\ASUP\CfgMarkerBrushes\data\lines_vertical_small_ca.paa";
+        scope = 1;
+        drawBorder = 0;
+    };
+
+    class ASUP_Squares_Big {
+        name = $STR_ASUP_CfgMarkerBrushes_Squares_Big;
+        texture = "\ASUP\CfgMarkerBrushes\data\squares_big_ca.paa";
+        scope = 1;
+        drawBorder = 0;
+    };
+    class ASUP_Squares_Medium {
+        name = $STR_ASUP_CfgMarkerBrushes_Squares_Medium;
+        texture = "\ASUP\CfgMarkerBrushes\data\squares_medium_ca.paa";
+        scope = 1;
+        drawBorder = 0;
+    };
+    class ASUP_Squares_Small {
+        name = $STR_ASUP_CfgMarkerBrushes_Squares_Small;
+        texture = "\ASUP\CfgMarkerBrushes\data\squares_small_ca.paa";
+        scope = 1;
+        drawBorder = 0;
+    };
+
+    class ASUP_PrelinNoise_128 {
+        name = $STR_ASUP_CfgMarkerBrushes_PerlinNoise_128;
+        texture = "#(ai,256,256,1)perlinNoise(128,128,0.2,0.8)";
+        scope = 1;
+        drawBorder = 0;
+    };
+    class ASUP_PrelinNoise_256 {
+        name = $STR_ASUP_CfgMarkerBrushes_PerlinNoise_256;
+        texture = "#(ai,256,256,1)perlinNoise(256,256,0.2,0.8)";
+        scope = 1;
+        drawBorder = 0;
+    };
+};
